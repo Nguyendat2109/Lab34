51762d75db435b617d8ece3c9d2ee67fd368b0ec
Nguyendat2109 <dat21092003@gmail.com>
2024-01-28 00:30:18 +0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+var Cars=require('../models/cars');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+router.get('/',async(req,res,next)=>{
+  try {
+      var data=await Cars.find().populate("_id");
+      res.json({ 
+      'status':200,
+      'mes':"list success",
+      'data':data
+
+      })
+    
+  } catch (error) {
+      console.log(error);
+  }
+})
 
 module.exports = router;
