638cbdd839c9007b0cbda45dedfa307d820eb531
Nguyendat2109 <dat21092003@gmail.com>
2024-01-28 01:00:46 +0700

    fix mongoose

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 var mongoose=require('mongoose');
 mongoose.set('strictQuery',true);
 var local='mongodb://127.0.0.1:27017/Lab34';
+var url='mongodb+srv://dat21092003:At6sJcibARcr54fT@lab34.dj5lsik.mongodb.net/?retryWrites=true&w=majority';
 var connect=async()=>{
     try {
-        await mongoose.connect(local,{});
+        await mongoose.connect(url,{});
         console.log("connect success");
     } catch (error) {
         console.log(error);
